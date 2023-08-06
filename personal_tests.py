cc79cc541e4dd4ee171094a7cb7e959c92386ec5
Ben Campbell <ben.campbell.w@gmail.com>
2023-08-06 18:57:47 -0400

    modified testing code

--- personal_tests.py
+++ personal_tests.py
@@ -8,13 +8,21 @@ tests = dict([
     ['A sentence with Capital letters.', 'f xjsyjshj bnym hfunyfq qjyyjwx.'],
     ['#$%^&*()', '#$%^&*()']
     ])
+# These are the same tests used in the tests.py file.
 
+##Old code: This behaved the way I expected it to.
 # for plaintext, cryptotext in tests.items():
 #     print("PLAIN", plaintext)
 #     print("CRYPT", cryptotext)
 #     print("SHFT5", shift5(plaintext))
 
-CONDITION = True
 for P, C in tests.items():
-    CONDITION = (True if shift5(P) == C else False)
+    if shift5(P) == C:
+        CONDITION = True
+    else:
+        CONDITION = False
+        break
 print(CONDITION)
+#CONDITION should be true if all the tests pass.
+
+#This code worked as expected for me. I think my code is working fine? Very confusing why I'm still getting errors on github tests.
